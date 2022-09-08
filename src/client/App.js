be8ba359ad1d7d5d6a2c2a1167c4e5f248752427
Dennis P George <dennisgeorge.mec@gmail.com>
2022-09-08 19:20:24 -0400

    Add React-Router

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './app.css';
-import ReactImage from './react.png';
 
-export default class App extends Component {
-  state = { username: null };
+const Sample = () => (
+  <div>
+    <p>Sample</p>
+  </div>
+);
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+const Sample2 = () => (
+  <div>
+    <p>Sample2</p>
+  </div>
+);
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
-}
+export default () => (
+  <Routes>
+    <Route path="/sample" element={<Sample2 />} />
+    <Route path="/" element={<Sample />} />
+  </Routes>
+);
