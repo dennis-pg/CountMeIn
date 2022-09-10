3010209b8973fd36b8e2cf9fdbc6a81081cbf8de
Dennis P George <dennisgeorge.mec@gmail.com>
2022-09-09 20:21:13 -0400

    Fix rebase

--- src/client/App.js
+++ src/client/App.js
@@ -5,13 +5,9 @@ import { ThemeProvider } from '@emotion/react';
 import { createTheme } from '@mui/material';
 import Navbar from './shared/components/Navbar';
 import Login from './pages/login/index';
-<<<<<<< HEAD
 import { AuthProvider } from './contexts/AuthContext';
-import ManageDataPoints from './pages/member/DataPoint/index';
-=======
 import ManageDataPoints from './pages/member/DataPoint';
 import SelectDataPoints from './pages/buyer/SelectDataPoints';
->>>>>>> b667b68... Select Data Points Screen added
 
 const theme = createTheme({
   palette: {
@@ -30,21 +26,13 @@ const Sample = () => (
 
 export default () => (
   <ThemeProvider theme={theme}>
-<<<<<<< HEAD
     <AuthProvider>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/manage-data-points" element={<ManageDataPoints />} />
+        <Route path="/select-data-points" element={<SelectDataPoints />} />
         <Route path="/" element={<Sample />} />
       </Routes>
     </AuthProvider>
-=======
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/manage-data-points" element={<ManageDataPoints />} />
-      <Route path="/select-data-points" element={<SelectDataPoints />} />
-      <Route path="/" element={<Sample />} />
-    </Routes>
->>>>>>> b667b68... Select Data Points Screen added
   </ThemeProvider>
 );
