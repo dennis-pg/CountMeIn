33439f42d5ebb5e176ee23aa5865888813a1add1
boatcow <siddarthsairaj@gmail.com>
2022-09-10 11:04:42 -0400

    member change

--- src/client/FirestoreMember.js
+++ src/client/FirestoreMember.js
@@ -1,98 +1,74 @@
-import React, { useState, useEffect, Fragment, useContext } from 'react';
-import firebase from './firebase';
+import React, {
+  useState, useEffect, Fragment, useContext
+} from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { useAuth } from "./contexts/AuthContext"
+import firebase from './firebase';
+import { useAuth } from './contexts/AuthContext';
 
-function FirestoreMember() {
-  const { currentUser } = useAuth()
-  const currentUserId = currentUser ? currentUser.uid : null;
-  const [member, setMember] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [gender, setGender] = useState('');
-  const [age, setAge] = useState('');
-  const [academiaAccess, setAcademiaAccess] = useState(False);
-  const [bpAccess, setBpAccess] = useState('');
+export const memberRef = firebase.firestore().collection('Member');
 
-  const memberRef = firebase.firestore().collection('Member');
-
-  //REALTIME GET FUNCTION
-  function getUserMember() {
-    setLoading(true);
-    memberRef
-      .onSnapshot((querySnapshot) => {
-        const items = [];
-        querySnapshot.forEach((doc) => {
-          items.push(doc.data());
-        });
-        setMember(items);
-        setLoading(false);
-      });
-
-  }
-function getUserDetails(requestUserId) {
-  // setLoading(true);
-
-  memberRef
-    .onSnapshot((querySnapshot) => {
-      const items = [];
-      querySnapshot.forEach((doc) => {
-        items.push();
-        var c=doc.data();
-        if(c.user_id==requestUserId)
-        {
-        const dataPointsList = ['Blood Pressure', 'SpO2', 'RBC Count', 'SGPT', 'SGOT', 'Serum Creatinine', 'HDL-Cholestorol', 'LDL-Cholestorol', 'TSH'];
-        for (var i = 0; i < dataPointsList.length; i++)
-        {   
-            var key=dataPointsList[i]
-            var data_item={
-                "Data Point Name": key,
-                "Govt": {
-                    "access": c['Access']['Government']['master_access'] || c['Access']['Government'][key],
-                    "basePrice": c['basePrice'][key]
-                },
-                "Commercial": {
-                    "access": c['Access']['Commercial']['master_access'] || c['Access']['Commercial'][key],
-                    "basePrice": c['basePrice'][key]
-                },
-                "Academia": {
-                    "access": c['Access']['Academia']['master_access'] || c['Access']['Academia'][key],
-                    "basePrice": c['basePrice'][key]
-                }
-            }
-            items.push(data_item);
-        }   
-        }
-        console.log(items);
-        return items;
-      });
-      setMember(items);
-      // setLoading(false);
+function getUserMember() {
+  memberRef.onSnapshot((querySnapshot) => {
+    const items = [];
+    querySnapshot.forEach((doc) => {
+      items.push(doc.data());
     });
-    return items;
+    setMember(items);
+  });
 }
 
+export function getUserDetails(requestUserId) {
+// setLoading(true);
+  memberRef.onSnapshot((querySnapshot) => {
+    const dataPointsObjArray = [];
+    querySnapshot.forEach((doc) => {
+      dataPointsObjArray.push();
+      const c = doc.data();
+      if (c.user_id == requestUserId) {
+        const dataPointsList = ['Blood Pressure', 'SpO2', 'RBC Count', 'SGPT', 'SGOT', 'Serum Creatinine', 'HDL-Cholestorol', 'LDL-Cholestorol', 'TSH'];
+        for (let i = 0; i < dataPointsList.length; i++) {
+          const key = dataPointsList[i];
+          const dataItem = {
+            'Data Point Name': key,
+            Govt: {
+              access: c.Access.Government.master_access || c.Access.Government[key],
+              basePrice: c.basePrice[key]
+            },
+            Commercial: {
+              access: c.Access.Commercial.master_access || c.Access.Commercial[key],
+              basePrice: c.basePrice[key]
+            },
+            Academia: {
+              access: c.Access.Academia.master_access || c.Access.Academia[key],
+              basePrice: c.basePrice[key]
+            }
+          };
+          dataPointsObjArray.push(dataItem);
+        }
+      }
+      console.log(dataPointsObjArray);
+      return dataPointsObjArray;
+    });
+
+    // setLoading(false);
+  });
 }
-  useEffect(() => {
-    getUserMember();
-    // eslint-disable-next-line
-  }, []);
-
-  // ADD FUNCTION
-  function addUserHealthMetric(userId,data) {
-    memberRef.doc(userId).set({foo:'bar'}, {merge: true})
-  }
 
 
-  // EDIT FUNCTION
-  function editUserHealthMetric(userId,fieldToUpdate) {
-    setLoading();
-    memberMetricsRef
-      .doc(userId)
-      .update(fieldToUpdate)
-      .catch((err) => {
-        console.error(err);
-      });
-  }
+// ADD FUNCTION
+function addUserHealthMetric(userId, data) {
+  memberRef.doc(userId).set({ foo: 'bar' }, { merge: true });
 }
 
-export default FirestoreMember;
+
+// EDIT FUNCTION
+function editUserHealthMetric(userId, fieldToUpdate) {
+  memberMetricsRef
+    .doc(userId)
+    .update(fieldToUpdate)
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+export default getUserDetails;
