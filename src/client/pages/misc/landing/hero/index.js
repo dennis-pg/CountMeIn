c6e6e412faa6526015904bbba423aa1009d001ab
Dennis P George <dennisgeorge.mec@gmail.com>
2022-09-11 21:00:38 -0400

    Landing title key changed

--- src/client/pages/misc/landing/hero/index.js
+++ src/client/pages/misc/landing/hero/index.js
@@ -4,43 +4,47 @@ import {
 } from '@mui/material';
 import backgroundSrc from '../../../../../../public/images/White-Background.png';
 import graphicSrc from '../../../../../../public/images/Graphic.png';
+import { useTheme } from '@emotion/react';
 
 
-const Hero = () => (
-  <Grid
-    container
-    sx={{
-      height: '100vh',
-      backgroundImage: `url(${backgroundSrc})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundColor: t => (t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900]),
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    }}
-    alignItems="center"
-  >
-    <CssBaseline />
+const Hero = () => {
+  const theme = useTheme();
+  return (
     <Grid
-      item
-      xs={12}
-      sm={6}
-      md={6}
+      container
+      sx={{
+        height: '100vh',
+        backgroundImage: `url(${backgroundSrc})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundColor: t => (t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900]),
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+      alignItems="center"
     >
-      <Stack spacing={1} sx={{ ml: 15, pb: 10 }}>
-        <Typography variant="h1">Your Healthcare privacy</Typography>
-        <Typography variant="h1">is our goal</Typography>
-      </Stack>
+      <CssBaseline />
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        md={6}
+      >
+        <Stack spacing={6} sx={{ ml: 15, pb: 10 }}>
+          <Typography variant="h1">Power of Data in <span style={{ color: theme.palette.primary.main }}>Your</span> Hands!</Typography>
+          <Typography sx={{ fontSize:'2em' }}>Count Me In lets you own your data and you get <span style={{ color: theme.palette.primary.main, fontWeight: 700 }}> paid </span></Typography>
+        </Stack>
+      </Grid>
+      <Grid
+        item
+        xs={false}
+        sm={6}
+        md={6}
+        square
+      >
+        <img src={graphicSrc} alt="Graphic" style={{ width: '50em', filter: 'drop-shadow(5px 5px 5px #CCC)' }} />
+      </Grid>
     </Grid>
-    <Grid
-      item
-      xs={false}
-      sm={6}
-      md={6}
-      square
-    >
-      <img src={graphicSrc} alt="Graphic" style={{ width: '50em', filter: 'drop-shadow(5px 5px 5px #CCC)' }} />
-    </Grid>
-  </Grid>
-);
+  )
+};
 
 export default Hero;
